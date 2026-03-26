bef481d761b37b4857aa7c65ad4f95f1283647dc
Protik Datta <protikdatta135@gmail.com>
2026-03-26 14:05:41 +0600

    generate page responsive

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Footer from '../components/common/Footer'
+import Navbar from '../components/common/Navbar'
 
 const History = () => {
   return (
-    <div>History</div>
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
   )
 }
 
